c21de7ed77f2baf794831c76a3894c3afebe2364
Kevin <32113593+repneuable@users.noreply.github.com>
2020-03-22 20:47:35 -0400

    Update M9HW.R

--- M9HW.R
+++ M9HW.R
@@ -38,28 +38,3 @@ plot_img %>%
   image_composite(logo, offset="+594+366")
 
 
-# 5 Principles of Design
-# (1) Alignment
-#  The purpose of alignment is to create order and organisation among elements.
-#  I believe this was achieved in this plot with the mirroring of the title and
-#  logo, as well as the centering of the axis titles and legends. 
-
-# (2) Repitition
-# Repition is used to create association and consistency between elements. This
-# was achieved here with the consistency of the color palette and smoothing method.
-# The opportunity to use repitition is greater when considering a portfolio of plots. 
-
-# (3) Contrast
-# Constrast is achieved here with the size of the Girth variable and brightness of the
-# smoothing line.
-
-# (4) Proximity
-# Proximity means that elements are visually connected in some way. This is achieved 
-# by grouping the legends and placing the title and logo on opposite corners. 
-
-# (5) Balance
-# Balance brings stabilization and structure to the plot with the mirroring of the 
-# logo and title and centering of the axis labels. Symmetry is difficulty to introduce
-# without altering the form of the graph. 
-
-
